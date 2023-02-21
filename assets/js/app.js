1a880b97bd0f1f4008a2506531a40040c1713fdf
Jomel Bautista <jomelgbautista@gmail.com>
2023-02-20 17:24:57 -0800

    updated content

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,13 @@
 window.onscroll = function() {
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0 ) {
       document.getElementById('navbar').classList.add('scrolled');
-  } else {
-      document.getElementById('navbar').classList.remove('scrolled');
   }
 }
+
+const navbarToggler = document.getElementById("navbar-toggler")
+navbarToggler.addEventListener("click", function() {
+  document.getElementById('navbar').classList.add('scrolled');
+})
 
 // Animate On Scroll
 AOS.init({
